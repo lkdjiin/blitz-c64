a3fa37b18263c9f29def4dda5ce0c2fb85f0e5c7
Xavier Nayrac <xavier.nayrac@gmail.com>
2025-11-20 18:02:35 +0100

    Extract constants

--- src/sound.asm
+++ src/sound.asm
@@ -1,45 +1,40 @@
 SOUND: {
 
-.const SIGVOL = 54296  // SID chip volume register
-.const ATDCY1 = 54277  // voice 1 attack/decay register
-.const SUREL1 = 54278  // voice 1 sustain/release register
-.const FRELO1 = 54272  // voice 1 frequency control (low byte)
-.const FREHI1 = 54273  // voice 1 frequency control (high byte)
-.const VCREG1 = 54276  // voice 1 control register
-.const JIFFLO = 162    // Low byte of jiffy clock
-
 // ---------------------------------------------------------------------
 beep: {
-  jsr SIDCLR // Clear the SID chip
+  jsr clear_v1 // Clear the SID chip
   lda #15    // Set the volume
-  sta SIGVOL
+  sta SID_VOLUME
   lda #0     // Set attack/decay
-  sta ATDCY1
+  sta SID_V1_ATTACK_DECAY
   lda #$f0   // Set sustain/release
-  sta SUREL1
+  sta SID_V1_SUSTAIN_RELEASE
   lda #132   // Set voice 1 frequency (low byte)
-  sta FRELO1
+  sta SID_V1_FREQUENCY_LOW
   lda #125   // Set voice 1 frequency (high byte)
-  sta FREHI1
+  sta SID_V1_FREQUENCY_HIGH
   lda #%00010001 // Select triangle waveform and gate sound
-  sta VCREG1
+  sta SID_V1_CONTROL
   lda #2     // Cause a delay of two jiffies
-  adc JIFFLO // Add current jiffy reading
-  DELAY:
-    cmp JIFFLO // and wait for two jiffies to elapse
-    bne DELAY
+  adc JIFFY_LOW // Add current jiffy reading
+  delay:
+    cmp JIFFY_LOW // and wait for two jiffies to elapse
+    bne delay
   lda #%00010000 // Ungate sound
-  sta VCREG1
+  sta SID_V1_CONTROL
   rts
 } // beep
 
+// ---------------------------------------------------------------------
 // Clear the SID chip.
-SIDCLR:
+clear_v1: {
   lda #0  // Fill with zeros
   ldy #24 // Index to FRELO1
-SIDLOP:
-  sta FRELO1,y // Store in SID chip address
-  dey          // For next lower byte
-  bpl SIDLOP   // Fill 25 bytes
+  loop:
+    sta SID_V1_FREQUENCY_LOW,y // Store in SID chip address
+    dey        // For next lower byte
+    bpl loop   // Fill 25 bytes
   rts
+} // clear_v1
+
 } // SOUND
